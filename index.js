72d40009987402a6380f29b2063fa970f3f02485
Drew Malin <drewmalin@gmail.com>
2021-03-02 17:24:24 -0800

    simplify

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-module.exports.handler = (request, response) => {
-  
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, Brev!\n');
-
+module.exports.handler = (request) => {
+  return 'Hello from NodeJS!';
 }
